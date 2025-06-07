a334e102872f40b8802f33f6f5aa48a6e590022c
Raghav9696 <raghavup123456@gmail.com>
2025-06-07 15:02:00 +0530

    Optimized
    Optimized from loopcount=2113
    Optimized from loopcount=1864
    Optimized from loopcount=907

--- api/primes/nprimes.py
+++ api/primes/nprimes.py
@@ -1,20 +1,19 @@
 n = 500
-loopcount=0
-primecount=1
-print(2,end=",")
-for i in range(3,n+1,2):
-    limit=i**.5
-    limit=int(limit)
-    isPrime=True
-    for j in range (2,limit+1):
-        loopcount+=1
-        if i % j==0:
-            isPrime=False
+loopcount = 0
+primecount = 1
+print(2, end=",")
+for i in range(3, n+1, 2):
+    limit = i**.5
+    limit = int(limit)
+    isPrime = True
+    for j in range(3, limit+1, 2):
+
+        loopcount += 1
+        if i % j == 0:
+            isPrime = False
             break
     if isPrime:
-        primecount+=1
-        print(i,end=",")
+        primecount += 1
+        print(i, end=",")
 
-print(f"\nloopcount={loopcount},primecount={primecount}",end=",")
-    
-
+print(f"\nloopcount={loopcount},primecount={primecount}", end=",")
