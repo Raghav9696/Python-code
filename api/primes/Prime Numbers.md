9e35c23776ecd1b88823ddb4d4b863dc55c99352
Raghav9696 <raghavup123456@gmail.com>
2025-06-07 14:57:30 +0530

    Optimized loopcount=2113 to loopcount=1864

--- api/primes/Prime Numbers.md
+++ api/primes/Prime Numbers.md
@@ -1,1 +1,5 @@
 ![alt text](image.png)# Python-code
+
+
+
+![alt text](image-1.png)
--- api/primes/nprimes.py
+++ api/primes/nprimes.py
@@ -1,7 +1,8 @@
 n = 500
 loopcount=0
-primecount=0
-for i in range(2,n+1):
+primecount=1
+print(2,end=",")
+for i in range(3,n+1,2):
     limit=i**.5
     limit=int(limit)
     isPrime=True
