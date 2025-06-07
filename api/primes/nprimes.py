4a21a606389ca5fc96a473150be1a4ee50d82c0e
Raghav9696 <raghavup123456@gmail.com>
2025-06-07 15:18:53 +0530

    Optimized completely
    Optimized from loopcount=2113
    Optimized from loopcount=1864
    Optimized from loopcount=907
    Optimized from loopcount=825
    Optimized from loopcount=374

--- api/primes/nprimes.py
+++ api/primes/nprimes.py
@@ -4,17 +4,24 @@ primecount = 3
 print(2, end=",")
 print(3, end=",")
 print(5, end=",")
-t=2
-i=5
-while i<n-1:
-    i+=t
-    t=6-t
+t = 2
+i = 5
+while i < n-1:
+    i += t
+    t = 6-t
     # print(i)
     # input()
+    if i % 3 == 0 or i % 5 == 0:
+        continue
     limit = i**.5
     limit = int(limit)
     isPrime = True
-    for j in range(3, limit+1, 2):
+    j = 5
+    t1 = 2
+    while j < limit-1:
+        j += t1
+        t1 = 6-t1
+    # for j in range(3, limit+1, 2):
 
         loopcount += 1
         if i % j == 0:
